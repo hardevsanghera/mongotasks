edc6ea591873c7d6b3355a53972eb2dc58cce4f8
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2022-01-14 15:24:59 -0800

    Update layout.blade.php

--- resources/views/books/layout.blade.php
+++ resources/views/books/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel Quickstart - Basic</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
